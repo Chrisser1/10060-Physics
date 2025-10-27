7c5f6057468bc07c7e03ffdb8c1f2eb8e92fe968
Chrisser1 <chrisgthomsen0310@gmail.com>
2025-10-27 16:59:51 +0100

    Added week 8

--- flake.nix
+++ flake.nix
@@ -10,28 +10,28 @@
   };
 
   outputs = { nixpkgs, pyproject-nix, ... }:
-    let
-      project = pyproject-nix.lib.project.loadPyproject {
-        projectRoot = ./.;
-      };
-
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      python = pkgs.python312;
-
-    in
-    {
-      devShells.x86_64-linux.default =
-        let
-          arg = project.renderers.withPackages { inherit python; };
-          pythonEnv = python.withPackages arg;
-
-        in
-        pkgs.mkShell { packages = [ pythonEnv ]; };
-
-      packages.x86_64-linux.default =
-        let
-          attrs = project.renderers.buildPythonPackage { inherit python; };
-        in
-        python.pkgs.buildPythonPackage (attrs // { env.CUSTOM_ENVVAR = "hello"; });
+  
+  let
+    project = pyproject-nix.lib.project.loadPyproject {
+      projectRoot = ./.;
     };
+
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    python = pkgs.python312;
+  in
+  {
+    devShells.x86_64-linux.default =
+      let
+        arg = project.renderers.withPackages { inherit python; };
+        pythonEnv = python.withPackages arg;
+
+      in
+      pkgs.mkShell { packages = [ pythonEnv ]; };
+
+    packages.x86_64-linux.default =
+      let
+        attrs = project.renderers.buildPythonPackage { inherit python; };
+      in
+      python.pkgs.buildPythonPackage (attrs // { env.CUSTOM_ENVVAR = "hello"; });
+  };
 }
